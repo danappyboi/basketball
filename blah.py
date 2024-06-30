d14a311ced6d6a7ad87a1d70015676a60b0b8a4e
Helios G <helios.gayibor@gmail.com>
2024-06-29 21:22:38 -0400

    i (think) adding shots works. got rid of removing for now

--- blah.py
+++ blah.py
@@ -7,6 +7,7 @@ shot2 = Shot(2, 3, True, 2)
 shot3 = Shot(1, 3, True, 3)
 
 dm.add_shot(shot1)
-dm.remove_shot(shot1)
+dm.add_shot(shot2)
+dm.add_shot(shot1)
 
 print(dm.get_shots())
--- data_manager.py
+++ data_manager.py
@@ -7,16 +7,20 @@ class Data_Manager:
         self.shots = self.__get_shots_from_data()
 
     def add_shot(self, shot : Shot):
-        self.shots.append(shot)
-        with open(self.data, "a") as csvfile:
+        """Adds shots to the data."""
+        # self.shots.append(shot)
+        with open(self.data, "a", newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(shot.to_data())
         
     def get_shots(self):
-        return self.shots
+        """Returns a list of all the shots."""
+        return self.__get_shots_from_data()
     
-    #TODO: doesn't work
-    def remove_shot(self, shot: Shot):
+    #TODO: doesn't work - best solution is csv -> list -> remove -> csv
+
+    def __remove_shot(self, shot: Shot):
+        """Removes a shot from the data."""
         if shot in self.shots:
             self.shots.remove(shot)
             with open(self.data, "r") as read:
